47d50f2c0faaed393868d984f2917a8f2049b52e
Monsotya <sonya20033009@gmail.com>
2021-09-16 11:05:21 +0300

    Modifyed 3

--- 3.py
+++ 3.py
@@ -8,28 +8,21 @@ if formula == "":
     isFormula = False
     print("\nresult = (" + str(isFormula) + ", None)")
 
-# checks if formula starts and ends in a number
-elif formula[0].isdigit() and formula[length].isdigit():
-    while length >= 0:
-        if formula[length].isdigit():
-            length -= 1
-        # checks if the expression contains wrong number of signs
-        elif formula[length] == '-' or formula[length] == '+':
-            if formula[length + 1] == '-' or formula[length + 1] == '+':
-                isFormula = False
-                print("\nresult = (" + str(isFormula) + ", None)")
-                break
-            length -= 1
-        # checks for other symbols
-        else:
-            isFormula = False
-            print("\nresult = (" + str(isFormula) + ", None)")
-            break
-else:
+# checks for wrong number of signs
+elif formula.find("++") != -1 or formula.find("--") != -1:
     isFormula = False
     print("\nresult = (" + str(isFormula) + ", None)")
 
-# if the expression is a formula, it`s calculated with eval()
-if isFormula:
-    result = eval(formula)
-    print("\nresult = (" + str(isFormula) + ", " + str(result) + ")")
+# checks if formula starts and ends in a number
+elif not(formula[0].isdigit() and formula[length].isdigit()):
+    isFormula = False
+    print("\nresult = (" + str(isFormula) + ", None)")
+else:
+    buffer = formula.replace("+", "")
+    buffer = buffer.replace("-", "")
+    if buffer.isdigit():
+        result = eval(formula)
+        print("\nresult = (" + str(isFormula) + ", " + str(result) + ")")
+    else:
+        isFormula = False
+        print("\nresult = (" + str(isFormula) + ", None)")
