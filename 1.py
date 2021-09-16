95224e371804b6bf0993ea487af934fcb99c9140
Monsotya <sonya20033009@gmail.com>
2021-09-16 13:19:10 +0300

    Update

--- 1.py
+++ 1.py
@@ -5,12 +5,12 @@ parser = argparse.ArgumentParser('Calculate given expression')
 parser.add_argument("expression", nargs='*')
 args = parser.parse_args()
 
-# using eval() to calcute given exression
+# using eval() to calculate given expression
 args.expression = "".join(args.expression)
 try:
     result = eval(args.expression)
     print(result)
 except SyntaxError:
-    print("\nError!")
+    print("\nError with string!")
 except NameError:
-    print("\nError!")
+    print("\nError with names!")
