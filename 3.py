22029ff3871c7ed339fb3766539aadc20d6fad79
Monsotya <sonya20033009@gmail.com>
2021-09-16 11:18:56 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,28 +1,26 @@
 formula = input("Enter expression\n")
 formula = formula.replace(" ", "")
-length = len(formula) - 1
 isFormula = True
 
 # checks if there is a line
 if formula == "":
     isFormula = False
-    print("\nresult = (" + str(isFormula) + ", None)")
 
 # checks for wrong number of signs
 elif formula.find("++") != -1 or formula.find("--") != -1:
     isFormula = False
-    print("\nresult = (" + str(isFormula) + ", None)")
 
 # checks if formula starts and ends in a number
-elif not(formula[0].isdigit() and formula[length].isdigit()):
+elif not (formula[0].isdigit() and formula[-1].isdigit()):
     isFormula = False
-    print("\nresult = (" + str(isFormula) + ", None)")
 else:
     buffer = formula.replace("+", "")
     buffer = buffer.replace("-", "")
-    if buffer.isdigit():
-        result = eval(formula)
-        print("\nresult = (" + str(isFormula) + ", " + str(result) + ")")
-    else:
+    if not buffer.isdigit():
         isFormula = False
-        print("\nresult = (" + str(isFormula) + ", None)")
+
+if isFormula:
+    result = eval(formula)
+    print("\nresult = (" + str(isFormula) + ", " + str(result) + ")")
+else:
+    print("\nresult = (" + str(isFormula) + ", None)")
